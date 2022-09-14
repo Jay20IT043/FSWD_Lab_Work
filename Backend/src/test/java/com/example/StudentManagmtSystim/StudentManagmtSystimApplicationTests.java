95f622497a81a935c908a6454e2b65af99a50e78
Jay20IT043 <88088549+Jay20IT043@users.noreply.github.com>
2022-09-14 13:16:40 +0530

    GEt,POST,PUT,DELETE Mapping

---
+++ Backend/src/test/java/com/example/StudentManagmtSystim/StudentManagmtSystimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagmtSystim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmtSystimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
